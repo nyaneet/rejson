234660e531558dccadd1275bf4366182f7d51762
Nikita Sermyagin <nyaneet@gmail.com>
2022-11-17 18:16:58 +0300

    add multi-arch

--- build.sh
+++ build.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 # rebuild RedisJSON image
 
-REDIS_VER=7.0.4
-REJSON_VER=v2.2.0
-RUST_VER=1.62.1
+REDIS_VER=7.0.5
+REJSON_VER=v2.4.0
+RUST_VER=1.65.0
 
-if [[ $(systemctl is-active docker) != 'active' ]]; then
-    echo "starting docker"
-    sudo systemctl start docker
-fi
-
-sudo docker buildx build \
+docker buildx build \
     --build-arg RUST_VER="$RUST_VER" \
     --build-arg REDIS_VER="$REDIS_VER" \
     --build-arg REJSON_VER="$REJSON_VER" \
-    --platform linux/arm64 \
-    -t bbilly1/rejson:"$REJSON_VER" \
-    -t bbilly1/rejson --push .
+    --platform linux/amd64,linux/arm/v7,linux/arm64 \
+    -t nyaaneet/rejson:"$REJSON_VER" \
+    -t nyaaneet/rejson --push .
 
-##
 exit 0
